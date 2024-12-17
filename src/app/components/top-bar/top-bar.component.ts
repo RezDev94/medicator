9e13372f7ef4c9731d96e5bee7c398c1e50e6296
Reza Tayebi <reza.tayebi73@gmail.com>
2024-12-17 13:50:24 +0330

    improvement: add delay to search input

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatButtonModule } from '@angular/material/button';
 import { SearchState } from '../../states/search.state';
+import { debounceTime } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
 	selector: 'app-top-bar',
@@ -23,13 +25,17 @@ import { SearchState } from '../../states/search.state';
 export class TopBarComponent implements OnInit {
 	fb = inject(FormBuilder);
 	searchKey$ = inject(SearchState).searchKey;
+	private destroyRef = inject(DestroyRef);
 
 	searchForm = this.fb.group({
 		key: this.fb.control('')
 	});
 
 	ngOnInit(): void {
-		this.searchForm.get('key')?.valueChanges.subscribe({
+		this.searchForm.get('key')?.valueChanges.pipe(
+			debounceTime(500),
+			takeUntilDestroyed(this.destroyRef)
+		).subscribe({
 			next: (val) => {
 				this.searchKey$.set(val ?? '');
 			}
