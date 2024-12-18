2b50992fd6d03dfea69b7ebe8fd52de5c3e5d321
Reza Tayebi <reza.tayebi73@gmail.com>
2024-12-18 15:47:26 +0330

    improvement: implement add and delete medication

--- src/app/components/medication-list/medication-list.component.ts
+++ src/app/components/medication-list/medication-list.component.ts
@@ -1,45 +1,20 @@
-import { Component, computed, inject } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, computed, DestroyRef, inject, OnInit, signal, ViewChild } from '@angular/core';
+import { MatTable, MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { SearchState } from '../../states/search.state';
 import { IMedication } from '@models/medication.model';
-import { DosageUnit } from '@enums/dosage-unit.enum';
-import { WeekDay } from '@enums/week-day.enum';
 import { DosagePipe } from '../../pipes/dosage/dosage.pipe';
 import { FrequencyPipe } from '../../pipes/frequency/frequency.pipe';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { AddMedicationComponent } from '../add-medication/add-medication.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { DataStoreService } from '../../services/data-store/data-store.service';
+import { MatIconModule } from '@angular/material/icon';
 
-const MEDICATION_DATA: IMedication[] = [
-	{
-		id: 1,
-		name: 'Aspirin',
-		dosage: {
-			value: 2,
-			unit: DosageUnit.TABLETS
-		},
-		frequency: {
-			day: [WeekDay.SATURDAY, WeekDay.THURSDAY],
-			time: ['12:30', '13:40']
-		},
-		createdAt: new Date(),
-		updatedAt: new Date()
-	},
-	{
-		id: 2,
-		name: 'Insulin',
-		dosage: {
-			value: 20,
-			unit: DosageUnit.MILLIGRAMS
-		},
-		frequency: {
-			day: 'all',
-			time: ['15:00']
-		},
-		createdAt: new Date(),
-		updatedAt: new Date()
-	},
-];
+const MEDICATION_DATA = signal<IMedication[]>([]);
 
 @Component({
 	selector: 'app-medication-list',
@@ -49,19 +24,55 @@ const MEDICATION_DATA: IMedication[] = [
 		MatCardModule,
 		DosagePipe,
 		FrequencyPipe,
-		TranslateModule
+		TranslateModule,
+		MatButtonModule,
+		MatIconModule
 	],
 	templateUrl: './medication-list.component.html',
 	styleUrl: './medication-list.component.scss',
 })
-export class MedicationListComponent {
-	searchKey$ = inject(SearchState).searchKey;
-	dataSource$ = computed(() => this.searchKey$() ? this.searchMedication() : MEDICATION_DATA);
+export class MedicationListComponent implements OnInit {
+	@ViewChild(MatTable) table!: MatTable<IMedication>;
 
-	displayedColumns: string[] = ['name', 'dosage', 'frequency', 'updatedAt'];
+	searchKey$ = inject(SearchState).searchKey;
+	dialog = inject(MatDialog);
+	dataStore = inject(DataStoreService);
+	private destroyRef = inject(DestroyRef);
+
+	dataSource$ = computed(() => this.searchKey$() ? this.searchMedication() : MEDICATION_DATA());
+
+	displayedColumns: string[] = ['name', 'dosage', 'frequency', 'updatedAt', 'action'];
+
+	ngOnInit(): void {
+		MEDICATION_DATA.set(this.dataStore.get());
+	}
 
 	private searchMedication(): IMedication[] {
 		// case insensitive search
-		return MEDICATION_DATA.filter((med) => med.name.toLowerCase().indexOf(this.searchKey$().toLowerCase()) !== -1);
+		return MEDICATION_DATA().filter((med) => med.name.toLowerCase().indexOf(this.searchKey$().toLowerCase()) !== -1);
+	}
+
+	addMedication(): void {
+		const dialogRef = this.dialog.open(AddMedicationComponent, {
+			width: '700px',
+			disableClose: true,
+			closeOnNavigation: true
+		});
+
+		dialogRef.afterClosed().pipe(
+			takeUntilDestroyed(this.destroyRef)
+		).subscribe(result => {
+			if (result) {
+				this.dataStore.store(result);
+				MEDICATION_DATA.set(this.dataStore.get());
+				this.table.renderRows();
+			}
+		});
+	}
+
+	removeMedication(id: number): void {
+		this.dataStore.remove(id);
+		MEDICATION_DATA.set(this.dataStore.get());
+		this.table.renderRows();
 	}
 }
